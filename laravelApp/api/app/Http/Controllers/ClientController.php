77a14a2fd5b8e73f200f5c56c2568c761579a81c
gonscenna <gonscenna@gmail.com>
2016-05-25 11:44:41 -0300

    Varios cambios
    - ADD CLIENT terminado
    - Se agregó todas las validaciones para agregar un cliente. Sólo falta
    la validación del CUIT, pero la voy a hacer después para hacerla bien.
    - Se devuelven todos los mensajes de error bien.
    
    - Descargue la FONT que faltaba para cargarla localmente.
    
    - Cree un archivo fonts.css para meter las 3 fuentes que usamos juntas,
    y no cargar 3 archivos separados (para mejorar el tiempo de respuesta)
    
    TO DO:
    - Los errores de los controllers manejarlos con constantes

--- laravelApp/api/app/Http/Controllers/ClientController.php
+++ laravelApp/api/app/Http/Controllers/ClientController.php
@@ -13,6 +13,12 @@ require app_path().'/validators.php';
 use DB;
 use Illuminate\Database\DatabaseManager;
 
+/*
+    TO DO LIST:
+
+        - Manejar errores comunes con constantes.
+
+*/
 
 class ClientController extends Controller
 {
@@ -46,23 +52,32 @@ class ClientController extends Controller
     {
         // Validar
         $validator = Validator::make($request->all(), [
-            'user' => 'required|alpha_num|max:20',
-            'lastname' => 'required|alpha_spaces|max:30',
-            'name' => 'required|alpha_spaces|max:30',
-            'address' => 'max:30|alpha_num_spaces'
+            'user' => 'required|alpha_num|between:3,20',
+            'lastname' => 'required|alpha_spaces|between:3,20',
+            'name' => 'required|alpha_spaces|between:3,20',
+            'address' => 'max:30|alpha_num_spaces',
+            'telephone1' => 'max:15|alpha_num_spaces',
+            'telephone2' => 'max:15|alpha_num_spaces',
+            'email' => 'email|max:30'
         ]);
 
         // Compruebo mensajes. Con $messages->has('field') sabes si el validator fallo para ese field
         if ($validator->fails()) { 
             $messages = $validator->messages();
             if ($messages->has('user'))
-                $response[] = array("error"=>"Usuario incorrecto");
-            if ($messages->has('address'))
-                $response[] = array("error"=>"Dirección incorrecta");   
+                $response[] = array("error"=>"El nombre de usuario debe tener entre 3 y 20 caractéres alfanuméricos sin espacios.");
             if ($messages->has('name'))
-                $response[] = array("error"=>"Nombre incorrecto");
+                $response[] = array("error"=>"El nombre debe tener entre 3 y 20 caractéres alfanuméricos.");
             if ($messages->has('lastname'))
-                $response[] = array("error"=>"Apellido incorrecto");    
+                $response[] = array("error"=>"El apellido debe tener entre 3 y 20 caractéres alfanuméricos.");
+            if ($messages->has('address'))
+                $response[] = array("error"=>"La dirección puede tener un máximo de 20 caractéres alfanuméricos.");
+            if ($messages->has('telephone1'))
+                $response[] = array("error"=>"El teléfono 1 puede tener un máximo de 15 caractéres alfanuméricos.");
+            if ($messages->has('telephone'))
+                $response[] = array("error"=>"El teléfono 2 puede tener un máximo de 15 caractéres alfanuméricos.");  
+            if ($messages->has('email'))
+                $response[] = array("error"=>"El correo electrónico debe tener formato de e-mail. y hasta 30 caractéres."); 
             return response()->json($response);
         }
 
@@ -84,7 +99,7 @@ class ClientController extends Controller
                 // Si es 23000 el codigo, es porque hay violacion de integridad. Habiendo validado los campos requeridos arriba,
                 // Supongo que lo unico que puede pasar es que el username exista, y al ser unique tira error
                 DB::rollback();
-                $response[] = array("error"=>"Nombre de usuario existente.");
+                $response[] = array("error"=>"El nombre de usuario elegido ya existe en la base de datos.");
                 return response()->json($response);
             }
             else
@@ -114,24 +129,19 @@ class ClientController extends Controller
         }
         catch (\Exception $e) {
             // Hubo error agregando
+            // Solo puede error de conexión en este punto, ya que los datos ya están validados y no hay ningún campo UNIQUE.
             $errorCode = $e->getCode();
-            if ($errorCode == 23000) {
-                // Si es 23000 el codigo, es porque hay violacion de integridad. Habiendo validado los campos requeridos arriba,
-                // Supongo que lo unico que puede pasar es que el username exista, y al ser unique tira error
-                DB::rollback();
-                return "entrada duplicada, pedir que cambie el username";
-            }
-            else
-                if ($errorCode == 2002 || $errorCode == 1044 || $errorCode== 1049)
-                    // Si es 2002, es porque no se pudo conectar. No tiro rollback() porque lanza otra vez excepcion porque no esta conectado
-                    // Si es 1044, usuario incorrecto
-                    // Si es 1049, no existe la tabla
-                    return "error de conexion";
+            if ($errorCode == 2002 || $errorCode == 1044 || $errorCode== 1049)
+                // Si es 2002, es porque no se pudo conectar. No tiro rollback() porque lanza otra vez excepcion porque no esta conectado
+                // Si es 1044, usuario incorrecto
+                // Si es 1049, no existe la tabla
+                $response[] = array("error"=>"Error de conexión a la base de datos.");
                 else {
-                    // Agarro cualquier otro error por las dudas
-                    DB::rollback();
-                    return "error ".$errorCode." ".$e->getMessage();;
-                }
+                // Agarro cualquier otro error por las dudas
+                DB::rollback();
+                $response[] = array("error"=>"Ha ocurrido un error inesperado. Contacte al administrador.");
+            }
+            return response()->json($response);
         }
     }
 

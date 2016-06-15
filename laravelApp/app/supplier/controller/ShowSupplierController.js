987e45066fd7edb91a7c59fd8d8fff701212e1ab
Juan Subiros <juanpablosubiros@avalith.net>
2016-06-15 12:10:01 -0300

    Agregado proveedor Alta, Busqueda y muestra de un proveedor

---
+++ laravelApp/app/supplier/controller/ShowSupplierController.js
@@ -0,0 +1,81 @@
+app.controller("ShowSupplierController", ["$scope", "supplierFactory", "$routeParams", function($scope, supplierFactory, $routeParams) {
+    /*
+        Initialize object form.
+    */
+    $scope.initialize = function () {
+        $scope.supplier = {
+            "description": "",
+            "tel": "",
+            "address": "",
+            "email": "",
+            "responsible": ""
+        };
+        $scope.savedSupplier = {
+            "description": "",
+            "tel": "",
+            "address": "",
+            "email": "",
+            "responsible": ""
+        };
+        
+    };
+    $scope.restoreData = function() {
+        $scope.supplier = angular.copy($scope.savedSupplier);
+    }
+    $scope.getSupplier = function () {
+        $scope.initialize();
+        $scope.loading = true;
+        $scope.errorLoading = false;
+
+        supplerFactory.getSupplier($routeParams.id).then(function (response) {
+                if (!response.error) {
+                    // If status=200 && No error msg.
+                    $scope.supplier = response.supplier;
+                    $scope.savedSupplier = angular.copy($scope.supplier);
+                   
+                }
+                else {
+                    // Error, show error box.
+                    $scope.errorLoading = true;
+                    $scope.errorResponse = response.error;
+                }
+                // Disable loading overlay
+                $scope.loading = false;
+            }, function (error) {
+                // HTTP Error. Force status msg, show error box, disable loading overlay.
+                $scope.errorResponse = "Error inesperado. Consulte al administrador";
+                $scope.loading = false;
+                $scope.errorLoading = true;
+        });
+    };
+    $scope.updateSupplier = function () {
+        $scope.updating = true;
+        $scope.errorUpdate = false;
+        $scope.successUpdate = false;
+        $scope.errors = [];
+
+        supplierFactory.updateSupplier($routeParams.id, $scope.supplier).then(function (response) {
+                if (response.id) {
+                    // If status=200 && No error msg.
+                    $scope.savedSupplier = angular.copy($scope.supplier);
+                    $scope.successUpdate = true;
+                }
+                else {
+                    // Error, show error box.
+                    $scope.errors = response;
+                    $scope.errorUpdate = true;
+                }
+                // Disable loading overlay
+                $scope.updating = false;
+            }, function (error) {
+                // HTTP Error. Force status msg, show error box, disable loading overlay.
+                $scope.errorResponse = "Error inesperado. Consulte al administrador";
+                $scope.updating = false;
+                $scope.error = true;
+        });
+    };
+    $scope.updating = false;
+    $scope.error = false;
+    $scope.errorResponse = "";
+    $scope.getSupplier();
+}]);
